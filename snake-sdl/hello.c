6af36734db9ff3c5c23558bef48f449384b49f81
Luc Sargent <lucsarge@gmail.com>
2025-05-08 15:03:22 -0800

    introduced timer and direction system to move snake every second

--- snake-sdl/hello.c
+++ snake-sdl/hello.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 #include <SDL3/SDL.h>
 #define SDL_MAIN_USE_CALLBACKS
@@ -14,13 +15,26 @@ static SDL_Renderer* renderer = NULL;
 const int tileSize = 40; // number of pixels each tile the board is wide
 int tileMargin = 3; // empty space around each tile when draw to help distinguish tiles
 
-struct SnakePart {
-    int x; // x coordinate of the snake head
-    int y; // y coordinate of the snake head
+struct Tile {
+    int x;
+    int y;
 };
 
-struct SnakePart head;
+struct Tile body[192] = { NULL }; // (Width / TileSize) * (Height / TileSize), max number of tiles on the board should mean the max number of snake body parts total
+int bodyCount;
 
+enum Direction {
+    NONE = 0,
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST,
+};
+
+enum Direction currDir = EAST;
+
+// timing variables
+unsigned int lastTime = 0, currentTime;
 
 void manageInput(SDL_Event *e) {
     int vInput = 0;
@@ -29,29 +43,23 @@ void manageInput(SDL_Event *e) {
     SDL_assert(e->type == SDL_EVENT_KEY_DOWN); // this assert was made in an example for the basic input handling, might not be necessary to keep
     if (e->key.scancode == SDL_SCANCODE_W) {
         printf("'W' key was pressed down\n");
-        vInput -= 1;
+        currDir = NORTH;
     }
     else if (e->key.scancode == SDL_SCANCODE_A) {
         printf("'A' key was pressed down\n");
-        hInput -= 1;
+        currDir = WEST;
     }
     else if (e->key.scancode == SDL_SCANCODE_S) {
         printf("'S' key was pressed down\n");
-        vInput += 1;
+        currDir = SOUTH;
     }
     else if (e->key.scancode == SDL_SCANCODE_D) {
         printf("'D' key was pressed down\n");
-        hInput += 1;
-    }
-
-    if (vInput != 0) {
-        head.y += 40 * vInput;
-    }
-    else if (hInput != 0) {
-        head.x += 40 * hInput;
+        currDir = EAST;
     }
 }
 
+// renders the snake and the apple
 void renderBoard() {
     // clear the board
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE); // the next SDL_RenderClear call uses the current render draw color so set it to black here
@@ -61,14 +69,43 @@ void renderBoard() {
     SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
 
     int tileRenderSize = tileSize - (tileMargin * 2);
-    SDL_FRect rect = { head.x + tileMargin, head.y + tileMargin, tileRenderSize, tileRenderSize };
 
-    SDL_RenderFillRect(renderer, &rect);
+    SDL_FRect bodyTile = { 0, 0, tileRenderSize, tileRenderSize };
+
+    bodyTile.x += tileSize * 2 + tileMargin;
+    bodyTile.y += tileSize * 2 + tileMargin;
+
+    for (int i = 0; i < bodyCount; i++) {
+        bodyTile.x = body[i].x + tileMargin;
+        bodyTile.y = body[i].y + tileMargin;
+        SDL_RenderFillRect(renderer, &bodyTile);
+    }
 
     // set color to red for drawing the apple
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
 
     SDL_RenderPresent(renderer);
+}
+
+void addBodyPart() {
+    // add a body part and increment the bodyCount by 1
+
+    bodyCount++;
+}
+
+void moveSnake() {
+    if (currDir == NORTH) {
+        body[0].y -= tileSize;
+    }
+    else if (currDir == EAST) {
+        body[0].x += tileSize;
+    }
+    else if (currDir == SOUTH) {
+        body[0].y += tileSize;
+    }
+    else if (currDir == WEST) {
+        body[0].x -= tileSize;
+    }
 }
 
 #pragma region SDL Callbacks
@@ -83,11 +120,21 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv) {
         return SDL_APP_FAILURE;
     }
 
+    bodyCount = 1; // start at 1 since the game with always begin with the snakes head, or body[0]
+
+    renderBoard(); // call once here when the game starts other wise the board won't render until after the first second
+
     return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppIterate(void* appstate) {
-    renderBoard();
+    currentTime = SDL_GetTicks();
+    if (currentTime > lastTime + 1000) {
+        moveSnake();
+        renderBoard();
+        printf("Tick %s\n", time);
+        lastTime = currentTime;
+    }
 
     return SDL_APP_CONTINUE;
 }
